dd0c97b30c39eb9f30936613c650dd6353d90333
Miguel Solano <miguel.abreu03@gmail.com>
2020-10-02 11:11:14 -0400

    First Selenium program "google search"

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
 async function example() {
-    let driver = await Builder().forBrowser("firefox").build();
+    let driver = await new Builder().forBrowser("firefox").build();
+    await driver.get("http://google.com");
+    await driver.findElement(By.name("q")).sendKeys("Selenium",Key.RETURN);
+
 }
+
+example();
