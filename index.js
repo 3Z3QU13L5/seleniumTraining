8554362c3c10eb1be73fa1dc8996d2efc143d6a8
Miguel Solano <miguel.abreu03@gmail.com>
2020-10-02 12:50:23 -0400

    Webdriver Code: open, check title and close

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
-async function example() {
-    let driver = await new Builder().forBrowser("firefox").build();
-    await driver.get("http://google.com");
-    await driver.findElement(By.name("q")).sendKeys("Selenium",Key.RETURN);
 
+async function main() {
+
+    //Declaration of variables
+    var baseURL = "https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers";
+    var expectedTitle = "Amazon.com Best Sellers: The most popular items on Amazon";
+    var actualTitle = "";
+
+    //declare and build webdriver instance
+    let driver = await new Builder().forBrowser("firefox").build();
+
+    //launch browser and direct it to the base URL
+    await driver.get(baseURL);
+
+    //get the value of the page title
+    actualTitle = await driver.getTitle();
+
+    //Compare both title and print if it passed or failed
+    actualTitle === expectedTitle? console.log( "Test Passed!"):console.log("Test Failed!");
+
+    //Close the browser
+    await driver.close();
 }
 
-example();
+main();
